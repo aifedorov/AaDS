05008feaae434e3a27ff31ab00dc3c2132c674f4
alexander <fairk19@gmail.com>
2016-01-13 13:57:18 +0300

    Добавлены решения пары задач

--- brackets/brackets/main.cpp
+++ brackets/brackets/main.cpp
@@ -1,16 +1,57 @@
-//
-//  main.cpp
-//  brackets
-//
-//  Created by Александр on 11.01.16.
-//  Copyright © 2016 Alexandr Fedorov. All rights reserved.
-//
-
 #include <iostream>
-#include <stack>
+#include <vector>
+
+void grow(int* buffer, int* bufferSize)
+{
+    int newBufferSize = *bufferSize * 2;
+    int* newBuffer = new int[newBufferSize];
+    
+    for (int i = 0; i < *bufferSize; i++) {
+        newBuffer[i] = buffer[i];
+    }
+    
+    buffer = newBuffer;
+    *bufferSize = newBufferSize;
+}
+
+bool isEmpty (int top)
+{
+    return top == -1;
+}
+
+void push(int* buffer, int* bufferSize, int* top, int c)
+{
+    if (*top + 1 > *bufferSize) {
+        grow(buffer, bufferSize);
+    }
+    buffer[++(*top)] = c;
+}
+
+int pop(int* buffer, int* top)
+{
+    if (!isEmpty(*top)) {
+        return buffer[(*top)--];
+    } else {
+        return -1;
+    }
+    
+}
 
 int main(int argc, const char * argv[]) {
     
-    std::stack<int> s;
+    int top = -1;
+    int bufferSize = 1;
+    int* buffer = new int[bufferSize];
+    
+    std::vector<char> brackets;
+    char c;
+    while ((c = getchar()) != '\n'){
+        brackets.push_back(c);
+    }
+    
+    for (int i = 0; i < brackets.size(); i++) {
+        std::cout << brackets[i] << " ";
+    }
+    
     return 0;
 }
---
+++ insertSort/insertSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  insertSort
+//
+//  Created by Александр on 13.01.16.
+//  Copyright © 2016 Alexandr Fedorov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
