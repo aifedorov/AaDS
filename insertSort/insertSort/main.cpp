0b8df02be40653d08a0a7cdd411f27159fefe4c1
alexander <fairk19@gmail.com>
2016-01-13 13:57:36 +0300

    добавлена сортировка вставками

--- insertSort/insertSort/main.cpp
+++ insertSort/insertSort/main.cpp
@@ -1,15 +1,30 @@
-//
-//  main.cpp
-//  insertSort
-//
-//  Created by Александр on 13.01.16.
-//  Copyright © 2016 Alexandr Fedorov. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
 
+void insertionSort(std::vector<int>* array, int size)
+{
+    for (int i = 1; i < size; ++i) {
+        int tmp = (*array)[i];
+        
+        int j = i - 1;
+        for (; j >= 0 && (*array)[j] > tmp; --j) {
+            (*array)[j + 1] = (*array)[j];
+        }
+        (*array)[j + 1] = tmp;
+    }
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    int tmp;
+    std::vector<int> array;
+    
+    while(std::cin >> tmp)
+        array.push_back(tmp);
+    
+    insertionSort(&array, (int)array.size());
+    
+    for (int i = 0; i < array.size(); i++) {
+        std::cout << array[i] << " ";
+    }
     return 0;
 }
